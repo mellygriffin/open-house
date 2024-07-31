d85acc2fac52ab3ef0f70bc7f5776c8cc7dcd558
MellGriffin <missmelissagriffin@gmail.com>
2024-07-31 11:36:52 -0500

    Created listing Schema

---
+++ models/listing.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const listingSchema = new mongoose.Schema({
+    streetAddress: {
+        type: String,
+        required: true,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0,
+    },
+    size: {
+        type: Number,
+        required: true,
+        min: 0,
+    },
+});
